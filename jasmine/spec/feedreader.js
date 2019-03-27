323c2b1335303da5c0d50895b55eba466da91da0
Hsiao Li Lin <hsiaolilin@Hsiaos-MacBook-Air.local>
2019-03-27 10:10:50 -0400

    refactor function() { } to  () => {}

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,13 +1,13 @@
-$(function() {
-   
-    describe('RSS Feeds', function() {
-        
-        it('are defined', function() {
+$(function(){
+
+    describe('RSS Feeds', () => {
+
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-         it('each feed has a URL', function() {
+         it('each feed has a URL', () => {
              for (let feed of allFeeds){
                expect(feed.url).toBeDefined();
                expect(feed.url.length).not.toBe(0);
@@ -17,7 +17,7 @@ $(function() {
          });
 
 
-         it('each feed has a name', function() {
+         it('each feed has a name', () => {
              for (let feed of allFeeds){
                expect(feed.name).toBeDefined();
                expect(feed.name.length).not.toBe(0);
@@ -27,13 +27,13 @@ $(function() {
 
     });
 
-    describe('The menu', function() {
+    describe('The menu', () => {
 
-         it('hidden by default', function(){
+         it('hidden by default', () => {
               expect($('body').hasClass('menu-hidden')).toBeTruthy();
          });
 
-          it('changes visibility when icon is clicked', function(){
+          it('changes visibility when icon is clicked', () => {
               let menuIcon = $('.menu-icon-link');
               menuIcon.click();
               expect($('body').hasClass('menu-hidden')).toBeFalsy();
@@ -42,13 +42,13 @@ $(function() {
           });
     });
 
-    describe('Initial Entries', function() {
+    describe('Initial Entries', () => {
 
-         beforeEach(function(done){
+         beforeEach( (done) => {
            loadFeed(0,done);
          });
 
-         it ('has at least one entry in feed container', function(){
+         it ('has at least one entry in feed container', () => {
            let container = document.querySelector('.feed');
            let entries = container.querySelectorAll('.entry');
            expect(entries.length).toBeGreaterThan(0);
@@ -56,22 +56,22 @@ $(function() {
 
     });
 
-    describe('New Feed Selection', function(){
+    describe('New Feed Selection', () => {
 
          let feed1;
          let feed2;
 
-         beforeEach(function(done){
-           loadFeed(1, function(){
+         beforeEach( (done) => {
+           loadFeed(1, () => {
              feed1 = document.querySelector('.feed').innerHTML;
-             loadFeed(2, function(){
+             loadFeed(2, () => {
                feed2 = document.querySelector('.feed').innerHTML;
                done();
              });
            });
          })
 
-         it('new feed is loaded and the content changes', function(){
+         it('new feed is loaded and the content changes', () => {
            expect(feed1).toBeDefined();
            expect(feed2).toBeDefined();
            expect(feed1).not.toBe(feed2);
